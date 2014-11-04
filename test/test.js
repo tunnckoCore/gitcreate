d22ed1e04b5bbb48ee45fd865f2673d2ee6370f5
tunnckoCore <mamemto_100@mail.bg>
2014-11-04 19:31:22 +0200

    tweaks

---
+++ test/test.js
@@ -0,0 +1,44 @@
+/**
+ * gitcreate <https://github.com/tunnckoCore/gitcreate>
+ *
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+//var gitcreate = require('../index');
+
+describe('gitcreate', function () {
+
+  describe('should create repo', function () {
+    it('if 1st is `object` and dont have more args (promise:true)', function (done) {
+      done();
+    });
+    it('if 1st is `string`, 2nd `object`, 3rd `callback` (promise:false)', function (done) {
+      done();
+    });
+    it('if 1st is `string`, 2nd `object` (promise:true)', function (done) {
+      done();
+    });
+    it('if 1st is `object`, 2nd `callback` (promise:false)', function (done) {
+      done();
+    });
+  })
+
+  describe('should throw', function () {
+    it('if 1st is `object` and dont have more args (promise:false)', function (done) {
+      done();
+    });
+    it('if 1st is `string` and dont have more args (promise:false)', function (done) {
+      done();
+    });
+    it('if 1st is `string`, 2nd `object` (promise:false)', function (done) {
+      done();
+    });
+  })
+
+})
